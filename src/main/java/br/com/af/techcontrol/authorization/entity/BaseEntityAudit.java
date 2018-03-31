6be0a2cef28e6f3276b6b4323cd2124ca95f5376
Francis Oliveira <fnolivei@outlook.com>
2018-03-31 18:58:11 -0300

    refactoring

--- src/main/java/br/com/af/techcontrol/authorization/entity/BaseEntityAudit.java
+++ src/main/java/br/com/af/techcontrol/authorization/entity/BaseEntityAudit.java
@@ -11,6 +11,8 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 
 @EntityListeners(AuditingEntityListener.class)
@@ -20,18 +22,24 @@ public abstract class BaseEntityAudit extends BaseEntity {
 	private static final long serialVersionUID = 1L;
 
 	@Getter
+	@JsonIgnore
 	@CreatedBy
 	private String createdBy;
 
+	//@JsonFormat(pattern = "dd:MM:yyyy HH:mm:ss")
 	@Getter
+	@JsonIgnore
 	@CreatedDate
 	private LocalDateTime createdDate;
 
 	@Getter
+	@JsonIgnore
 	@LastModifiedBy
 	private String lastModifiedBy;
 
+	//@JsonFormat(pattern = "dd:MM:yyyy HH:mm:ss")
 	@Getter
+	@JsonIgnore
 	@LastModifiedDate
 	private LocalDateTime lastModifiedDate;
 
--- src/main/java/br/com/af/techcontrol/authorization/entity/Pessoa.java
+++ src/main/java/br/com/af/techcontrol/authorization/entity/Pessoa.java
@@ -12,7 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.PostLoad;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
@@ -69,25 +68,15 @@ public class Pessoa extends BaseEntityAudit {
 
 	@Getter
 	@Setter
-	@OneToOne(mappedBy = "pessoa")
-	private User user;
-
-	@Getter
-	@Setter
 	@ManyToMany(cascade = CascadeType.ALL)
 	@JoinTable(name = "pessoas_enderecos", joinColumns = @JoinColumn(name = "pessoa_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "endereco_id", referencedColumnName = "id"))
-	private List<Endereco> enderecos;
+	private List<Endereco> enderecos = new ArrayList<Endereco>();
 
 	@Getter
 	@Setter
 	@OneToMany
 	@JoinColumn(name = "pessoa_id", referencedColumnName = "id")
 	private List<Contato> contatos = new ArrayList<Contato>();
-
-	@NonNull
-	@Getter
-	@Setter
-	private Boolean isEnable;
 
 	@PrePersist
 	@PreUpdate
